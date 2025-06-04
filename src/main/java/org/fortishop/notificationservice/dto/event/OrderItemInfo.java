a1fe637ee12981da7153c39fb02b17c1bbe73809
Kyumin-Chang <kyumin0529@naver.com>
2025-06-04 15:55:49 +0900

    chore: 오류 수정

--- src/main/java/org/fortishop/notificationservice/dto/event/OrderItemInfo.java
+++ src/main/java/org/fortishop/notificationservice/dto/event/OrderItemInfo.java
@@ -5,7 +5,6 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-import org.fortishop.orderpaymentservice.domain.OrderItem;
 
 @Getter
 @Builder
